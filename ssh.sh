3187f10193113c483d49109887b95d08ec2db409
DanuraSanthush <danurasanthush@gmail.com>
2021-08-14 00:47:16 +0530

    Some Configuratios

--- ssh.sh
+++ ssh.sh
@@ -1,59 +1,4 @@
-#!/bin/bash
 
-#Font Colors
-
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-BLUE="\e[34m"
-CYAN="\e[36m"
-ENDCOLOR="\e[0m"
-
-clear
-
-#public ip
-
-pub_ip=$(wget -qO- https://ipecho.net/plain ; echo)
-
-#root check
-
-if ! [ $(id -u) = 0 ]; then
-   echo -e "${RED}Plese run the script with root privilages!${ENDCOLOR}"
-   exit 1
-fi
-
-spinner()
-{
-    #Loading spinner
-    local pid=$!
-    local delay=0.75
-    local spinstr='|/-\'
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
-    done
-    printf "    \b\b\b\b"
-}
-pre_req()
-{
-        #installing pre-requirements and adding port rules to ubuntu firewall
-		
-	apt update -y && apt upgrade -y
-
-        apt-get install -y dropbear && apt-get install -y stunnel4 && apt-get install -y squid && apt-get install -y cmake && apt-get install -y python3 && apt-get install -y screenfetch && apt-get install -y openssl
-        ufw allow 443/tcp
-	ufw allow 444/tcp
-        ufw allow 22/tcp
-        ufw allow 80/tcp
-        ufw allow 110/tcp
-        ufw allow 8080/tcp
-        ufw allow 7300/tcp
-        ufw allow 7300/udp
-}
-mid_conf()
 {
 
 #configuring openssh
@@ -106,7 +51,3 @@ chmod +x /usr/local/bin/menu
 }
 fun_service_start()
 {
-#enabling and starting all services
-#exit script
-echo -e "\n${CYAN}Script installed. You can access the panel using 'menu' command. ${ENDCOLOR}\n"
-echo -e "\nPress Enter key to exit"; read
